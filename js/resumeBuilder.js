06559489ba0dfe16c178fe4132caa1be4c96861d
Cameron <cs6970@att.com>
2015-01-16 19:45:09 -0500

    Add Built JSONs complete

---
+++ js/resumeBuilder.js
@@ -0,0 +1,86 @@
+var bio = {
+        "name": "Cameron Syme",
+        "role": "Developer",
+        "contact": {
+            "mobile": "617-839-0572",
+            "email": "camsyme@gmail.com",
+            "github": "csyme",
+            "twitter": "twitter",
+            "location": "North Andover, MA"
+    },
+        "skills": ["programming","engineering", "RAN Design" ],
+        "welcomeMessage": "Welcome to my resume",
+        "biopic": "images/fry.jpg",
+}
+
+var education = { 
+    "Schools": [
+        {
+            "name": "RMIT",
+            "location": "Melbourne, Vic, Australia",
+            "degree": "ASEE",
+            "majors": ["electronics", "physics"],
+            "dates": 1989,
+            "url": "http://RMIT.edu.au"
+        },
+        {
+            "name": "Swinburn University",
+            "location": "Hawthorn, Vic, Australia",
+            "degree": ["BSEE"],
+            "majors": ["Electonics", "Mathematics"],
+            "dates": 1995,
+            "url": "http://swinburne.edu.au"
+        }
+    ]
+}
+
+var onlineCourses = {
+            "title": "Into to LTE",
+            "school": "Award Solutions",
+            "date": "Jan 2009",
+            "url": "http://www.awardsolutions.com"
+        
+}
+
+
+var work = { 
+    "jobs": [
+        {
+            "employer": "AT&T",
+            "title": "RAN Design Manager",
+            "location": "Framingham, MA, US",
+            "dates": "2012-present",
+            "description": "Overseeing the design and overlay of LTE on 2600 sites. ",
+        },
+        {
+            "employer": "Lightsquared",
+            "title": "RAN Design Manager",
+            "location": "Herndon, VA, US",
+            "dates": "2011-2012",
+            "description": "",
+        },
+        {
+            "employer": "Clearwire",
+            "title": "Market Director",
+            "location": "Waltham, MA, US",
+            "dates": "2009-2011",
+            "description": "RAN Design Engineer",
+        }
+    ]
+}
+var projects = { 
+    "prjects": [
+        {   
+            "title": "Project 1 - Mockup Webpage",
+            "dates": "Dec 2014",
+            "description": "1st web page mockup for Udacity",
+            "images" : ["image/fry.jpg", "image/fry.jpg"]
+        },
+        {   
+            "title": "Project 2 - Interactive Resume",
+            "dates": "Jan 2015",
+            "description": "1st web page mockup for Udacity",
+            "images" : ["image/fry.jpg", "image/fry.jpg"]
+        }
+     ]    
+}
